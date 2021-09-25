54b2b0d10a2e05ada7a24a9c4bd970c2cd079395
kalan <kalanin.peto@gmail.com>
2021-09-25 09:15:18 +0200

    feat: init app request logger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const APP_PORT = 3000;
 
+const appLogger = function (req, res, next) {
+  console.log(`${new Date().toISOString()} -> ${req.method} ${req.url}`);
+  next();
+};
+
+app.use(appLogger);
+
 app.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}.`);
 });
