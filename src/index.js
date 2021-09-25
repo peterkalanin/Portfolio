be8047904e4321a5b33bd3d9d803e92d6a93df50
kalan <kalanin.peto@gmail.com>
2021-09-25 09:22:31 +0200

    feat: hbs init, index page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 const APP_PORT = 3000;
 
+app.set('view engine', 'hbs');
+
 const appLogger = function (req, res, next) {
   console.log(`${new Date().toISOString()} -> ${req.method} ${req.url}`);
   next();
 };
-
 app.use(appLogger);
+
+app.get('', (req, res) => {
+  res.render('index');
+});
 
 app.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}.`);
