52bd49cea6d9c2921018766d66bb9d63979c3d1f
kalan <kalanin.peto@gmail.com>
2021-12-24 09:09:47 +0100

    fix: navigation click close menu on mobile

--- public/js/button-events.js
+++ public/js/button-events.js
@@ -1,13 +1,19 @@
 const mobileMenuEl = $('#mobile-menu');
 const mobileMenuButtonEl = $('#mobile-menu-button');
 
-mobileMenuButtonEl.bind('click', (event) => {
+function closeNavbar() {
   mobileMenuButtonEl[0].ariaExpanded =
     mobileMenuButtonEl[0].ariaExpanded == 'false' ? 'true' : 'false';
 
   if (mobileMenuButtonEl[0].ariaExpanded == 'false') {
     mobileMenuEl.addClass('hidden');
+    mobileMenuEl.removeClass('absolute');
   } else {
     mobileMenuEl.removeClass('hidden');
+    mobileMenuEl.addClass('absolute');
   }
+}
+
+mobileMenuButtonEl.bind('click', (event) => {
+  closeNavbar();
 });
